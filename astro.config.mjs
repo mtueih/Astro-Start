7e065e3e6a5305d829fe19af8cf661d197f865d2
mtueih <mtueih.org@outlook.com>
2025-09-28 11:16:57 +0800

    ``` feat(astro.config): 添加 preact 集成支持
    在 astro 配置文件中引入并配置了 preact 集成，以支持 preact 组件的使用。
    ```

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.mtueih.com",
+  site: "https://www.mtueih.com",
+  integrations: [preact()],
 });
